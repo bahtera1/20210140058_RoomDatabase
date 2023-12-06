a0048718e84d226c1454c9ad633d568201c40479
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-12-06 20:44:32 +0700

    add DestinasiHome object di HalamanHome.kt

--- app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanHome.kt
@@ -1,4 +1,10 @@
 package com.example.activity7.ui.theme.halaman
 
-object HalamanHome {
+import com.example.activity7.R
+import com.example.activity7.navigasi.DestinasiNavigasi
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }

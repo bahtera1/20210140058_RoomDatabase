82aad173bab0b138550edf389220bd31557d4afa
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-12-06 20:46:37 +0700

    add Composable fun DataSiswa di HalamanHome.kt

--- app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanHome.kt
@@ -1,6 +1,9 @@
 package com.example.activity7.ui.theme.halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.activity7.R
+import com.example.activity7.data.Siswa
 import com.example.activity7.navigasi.DestinasiNavigasi
 
 
@@ -8,3 +11,9 @@ object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
 }
+
+@Composable
+fun DataSiswa(
+    siswa: Siswa,
+    modifier: Modifier = Modifier
+){}

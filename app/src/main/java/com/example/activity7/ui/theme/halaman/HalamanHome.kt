c8afe6f01efc6d6cb414b34fd153735107b8951e
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-12-06 20:50:35 +0700

    add Card And Column di HalamanHome.kt

--- app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanHome.kt
@@ -1,7 +1,13 @@
 package com.example.activity7.ui.theme.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import com.example.activity7.R
 import com.example.activity7.data.Siswa
 import com.example.activity7.navigasi.DestinasiNavigasi
@@ -16,4 +22,17 @@ object DestinasiHome : DestinasiNavigasi {
 fun DataSiswa(
     siswa: Siswa,
     modifier: Modifier = Modifier
-){}
+){
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+
+        }
+
+    }
+}

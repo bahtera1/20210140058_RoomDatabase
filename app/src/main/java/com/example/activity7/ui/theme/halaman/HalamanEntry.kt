5c5a6b21140d7fb0091408d04ec4b7a4821be83e
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-12-06 19:28:14 +0700

    add object DestinasiEntry di HalamanEntry.kt

--- app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanEntry.kt
@@ -1,4 +1,10 @@
 package com.example.activity7.ui.theme.halaman
 
-object HalamanEntry {
+import com.example.activity7.R
+import com.example.activity7.navigasi.DestinasiNavigasi
+
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }

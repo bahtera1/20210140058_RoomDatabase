7f1ef5689527899bdaa448cff1aadc4f1d0644f1
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-12-06 18:44:17 +0700

    add companion object in HomeViewModel.kt

--- app/src/main/java/com/example/activity7/model/HomeViewModel.kt
+++ app/src/main/java/com/example/activity7/model/HomeViewModel.kt
@@ -1,4 +1,10 @@
 package com.example.activity7.model
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.activity7.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }

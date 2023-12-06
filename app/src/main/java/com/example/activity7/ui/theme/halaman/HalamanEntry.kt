86ac11e1c8c59aeab602d4d2b915b7b563b1581e
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-12-06 20:14:57 +0700

    add outlinedtexfield untuk nama, alamat dan telepon di HalamanEntry.kt

--- app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanEntry.kt
@@ -2,12 +2,17 @@ package com.example.activity7.ui.theme.halaman
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.activity7.R
 import com.example.activity7.model.DetailSiswa
@@ -43,6 +48,32 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
     ) {
+
+        OutlinedTextField(
+            value = detailSiswa.nama,
+            onValueChange = {onValueChange(detailSiswa.copy(nama = it))},
+            label = { Text(stringResource(R.string.nama))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailSiswa.alamat,
+            onValueChange = {onValueChange(detailSiswa.copy(alamat = it))},
+            label = { Text(stringResource(R.string.alamat))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailSiswa.telpon,
+            onValueChange = {onValueChange(detailSiswa.copy(telpon = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(stringResource(R.string.telpon))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
 
     }
 }

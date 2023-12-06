d92c9bea5641e8f286a081890e9a435267496d98
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-12-06 19:43:18 +0700

    add column di HalamanEntry.kt

--- app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanEntry.kt
@@ -1,10 +1,13 @@
 package com.example.activity7.ui.theme.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.activity7.R
 import com.example.activity7.model.DetailSiswa
@@ -35,4 +38,12 @@ fun FormInputSiswa(
     detailSiswa: DetailSiswa,
     modifier: Modifier = Modifier,
     onValueChange: (DetailSiswa) -> Unit = {}
-){}
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+
+    }
+}
+

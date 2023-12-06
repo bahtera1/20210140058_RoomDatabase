776bdfb048a5b039d7ec4bbe6de89830b9448f29
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-12-06 19:32:49 +0700

    add fun EntrySiswaScreen di HalamanEntry.kt

--- app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/activity7/ui/theme/halaman/HalamanEntry.kt
@@ -1,6 +1,12 @@
 package com.example.activity7.ui.theme.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.activity7.R
+import com.example.activity7.model.EntryViewModel
+import com.example.activity7.model.PenyediaViewModel
 import com.example.activity7.navigasi.DestinasiNavigasi
 
 
@@ -8,3 +14,11 @@ object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigetback: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){}
